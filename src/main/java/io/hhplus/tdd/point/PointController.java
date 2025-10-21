9f0b057cded7411eda154e6bdfc90d7960a78644
Daehwan Kwak <dhfg0628@naver.com>
2025-10-21 22:37:08 +0900

    update PointController

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +21,15 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
-    }
+        UserPointTable userPointTable = new UserPointTable();
+
+        UserPoint userPoint = userPointTable.selectById(id);
+
+        return userPoint;
+
+        // return new UserPoint(0, 0, 0);
+
+    }  // point()
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
@@ -29,8 +38,15 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
-    }
+        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+
+        List<PointHistory> pointHistoryList = pointHistoryTable.selectAllByUserId(id);
+
+        return pointHistoryList;
+
+        // return List.of();
+
+    } // history()
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
