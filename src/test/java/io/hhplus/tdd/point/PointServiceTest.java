7d68a82f832976faca13bde6b95c951d031c2847
Daehwan Kwak <dhfg0628@naver.com>
2025-10-29 23:23:34 +0900

    update PointServiceTest.java

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -3,7 +3,10 @@ package io.hhplus.tdd.point;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 
-import static org.mockito.Mockito.mock;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PointServiceTest {
 
@@ -12,6 +15,23 @@ class PointServiceTest {
 
     private final PointService pointService = new PointService(userPointTable, pointHistoryTable);
 
+    @Test
+    @DisplayName("")
+    void 유저_포인트_조회_시_포인트가_없는_경우_기본_값으로_0포인트_반환() {
+
+        long id = 1L;
+
+        when(userPointTable.selectById(id)).thenReturn(UserPoint.empty(id));
+
+        UserPoint userPoint = pointService.searchPoint(id);
+
+        assertEquals(id, userPoint.id());
+        assertEquals(0, userPoint.point());
+
+        verify(userPointTable).selectById(id);
+        verifyNoInteractions(pointHistoryTable);
+
+    }
 
 
 
